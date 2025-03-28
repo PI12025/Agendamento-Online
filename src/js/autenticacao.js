f53d0d0659083be2b8616d9974f9ad9e9e2457c9
Jatniel S Souza <aprendasimplesjsz@gmail.com>
2025-03-28 11:21:43 -0300

    Primeiro commit do projeto integrador-1 PI12025 através de repositório local

---
+++ src/js/autenticacao.js
@@ -0,0 +1,34 @@
+//Acesse ou cadastre-se:
+$('.autenticacao').hide();
+
+let btn = document.querySelector('#autenticacao');
+btn.setAttribute('class', 'btn btn-lg btn-outline-dark pb-3 normal');
+
+btn.onclick = function () {
+    if ($("#autenticacao").hasClass("active")) {
+        btn.setAttribute('class', 'btn btn-lg btn-outline-dark pb-3 normal');
+    } else {
+        btn.setAttribute('class', 'btn btn-lg btn-outline-dark pb-3 active');
+    }
+}
+
+$('#autenticacao').click(function () {
+    $('.autenticacao').toggle();
+
+});
+
+//Eventos:
+//Entrar:
+$(document).ready(function () {
+    $("#entrar").click(function () {
+        $("#pagina3")[0].click();
+    });
+});
+//Avançar:
+$(document).ready(function () {
+    $("#avancar").click(function () {
+        $("#pagina2")[0].click();
+    });
+});
+
+
