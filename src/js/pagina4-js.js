490295a9a3f85c2a5454aadea8bc939b5d8cae2e
Jatniel S Souza <aprendasimplesjsz@gmail.com>
2025-04-06 22:58:22 -0300

    Ajustes na página4

--- src/js/pagina4-js.js
+++ src/js/pagina4-js.js
@@ -1,3 +1,4 @@
+//Calendário:
 const mesAtual = document.getElementById('mes&ano'),
     diaTag = document.querySelector('#dias'),
     iconeAntesDepois = document.querySelectorAll('.icone span');
@@ -5,34 +6,47 @@ const mesAtual = document.getElementById('mes&ano'),
 var meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
 
 let data = new Date(),
-    mes = data.getMonth(),
-    indMes = data.getMonth(),
-    ano = data.getFullYear();
+    mes = data.getMonth(),//3
+    ano = data.getFullYear();//2025
+
+/**new Date();
+new Date(valor);
+new Date(dataString);
+"YYYY-MM-DD"
+new Date(ano, mês, dia, hora, minuto, segundo, milissegundo); 
+
+dateObj.getDate() // O método getDate() retorna o dia do mês da data especificada de acordo com a hora local;
+
+dateObj.getDay() // O método getDay() retorna o dia da semana para a data especificada de acordo com a hora local, 
+onde 0 representa o Domingo.
+*/
+
+let teste = new Date(2025, 3, 30).getDay();
+console.log("Teste " + teste);
 
 function displayData() {
-    let diaAtual = new Date(ano, mes + 1, 0).getDate(), //abril - 30
-        primeirosDiasMes = new Date(ano, mes, 1).getDay(),//março - 2
-        ultimosDiasMesAnterior = new Date(ano, mes, 0).getDate(), //março - 31
-        primeirosDiasProxMes = new Date(ano, mes, diaAtual).getDay(); // maio - 3
+    let ultimoDiaMesAtual = new Date(ano, mes + 1, 0).getDate(),
+
+        diaSemanaPrimeiroDiaMesAtual = new Date(ano, mes, 1).getDay(),
+        ultimoDiaMesAnterior = new Date(ano, mes, 0).getDate(),
+
+        diaSemanaUltimoDiaMesAtual = new Date(ano, mes, ultimoDiaMesAtual).getDay();
+
     let liDia = "";
 
-    for (let i = primeirosDiasMes; i > 0; i--) {
-        let anterior = i === data.getDate();
-        liDia += `<li class="desativo">${ultimosDiasMesAnterior - i + 1}</li>`;
+    for (let i = diaSemanaPrimeiroDiaMesAtual; i > 0; i--) {
+        liDia += `<li class="desativo">${ultimoDiaMesAnterior - i + 1}</li>`;
     }
 
 
-    for (let i = 1; i <= diaAtual; i++) {
-
+    for (let i = 1; i <= ultimoDiaMesAtual; i++) {
         let hoje = i === data.getDate() && mes === new Date().getMonth() && ano === new Date().getFullYear() ? 'ativo cad' :
             i < data.getDate() && mes === new Date().getMonth() && ano === new Date().getFullYear() ? 'desativo' : 'cad';
-
         liDia += `<li class="${hoje}">${i}</li>`;
     }
 
-    for (let i = primeirosDiasProxMes; i < 6; i++) {
-        let anterior = i === data.getDate();
-        liDia += `<li class="desativo">${i - primeirosDiasProxMes + 1}</li>`;
+    for (let i = diaSemanaUltimoDiaMesAtual; i < 6; i++) {
+        liDia += `<li class="desativo">${i - diaSemanaUltimoDiaMesAtual + 1}</li>`;
     }
 
     mesAtual.innerText = `${meses[mes]} ${ano}`;
@@ -43,8 +57,7 @@ displayData();
 
 iconeAntesDepois.forEach(icone => {
     icone.addEventListener("click", () => {
-        let ind;
-        mes = icone.id === "antes" ? mes - 1 === indMes - 1 ? mes : mes - 1 : ind = mes + 1;
+        mes = icone.id === "antes" ? mes - 1 === new Date().getMonth() - 1 ? mes : mes - 1 : mes + 1;
 
         if (mes < 0 || mes > 11) {
             data = new Date(ano, mes),
